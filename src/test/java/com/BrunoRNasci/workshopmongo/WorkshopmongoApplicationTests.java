04309c717e479763843a942dea1abdd0f6d9609c
BrunoRNasci <brunoribeironasci@gmail.com>
2022-12-10 16:06:21 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/BrunoRNasci/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BrunoRNasci.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
